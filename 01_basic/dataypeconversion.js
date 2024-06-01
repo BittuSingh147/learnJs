1bbc9c0afb9316dda8e06681359a22216403ed20
19bcs1715 <19bcs1715@gmail.com>
2024-06-02 04:17:27 +0530

    conversion of datatypes

---
+++ 01_basic/dataypeconversion.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+number 33 output is number
+when we print  "33abc" it is convert into string
+*/
+
+//convert number into boolean//
+let loggedIn = 0;
+console.log(typeof loggedIn);
+
+let convertIntoBoolean = Boolean(loggedIn)
+console.log(typeof convertIntoBoolean);
+console.log(convertIntoBoolean);
+
+//convert number into string
+let num = 56
+console.log(typeof num)
+
+let ChangeIntoString = String(num)
+console.log(typeof ChangeIntoString);
+console.log(ChangeIntoString);
