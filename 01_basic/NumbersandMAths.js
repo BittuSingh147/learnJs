c705e80954ffd9b51eaf4013f31e3a147dfb75c0
19bcs1715 <19bcs1715@gmail.com>
2024-06-03 03:22:06 +0530

    Numbers and maths

---
+++ 01_basic/NumbersandMAths.js
@@ -0,0 +1,40 @@
+//Number
+// let value = 500;
+// console.log(value.toString());    // Outputs: "500"
+// console.log(typeof value);        // Outputs: "number"
+
+
+// Score = new Number(100)
+// console.log(Score)
+
+//ToExponentialMethod
+// let num = 12345.6789;
+// console.log(num.toExponential());      // Outputs: "1.23456789e+4"
+// console.log(num.toExponential(2));     // Outputs: "1.23e+4"
+// console.log(num.toExponential(4));     // Outputs: "1.2346e+4"
+// console.log(num.toExponential(6));     // Outputs: "1.234568e+4"
+
+//To fixed method 
+// let Num = 12345.6789
+// console.log(Num.toFixed(0))
+// console.log(Num.toFixed(3))
+// console.log(Num.toFixed(6)) // In this fixed method we can say that the number gives round of after decimal
+
+
+/******************************************** */
+//Math
+//let Digit = 4
+// console.log(Digit)
+// console.log(Math.abs(-4))//convert into positive number
+// console.log(Math.round(4.6))//it gives round of number fpr eg output is 5
+// console.log((Math.floor(4.3)));//it gives possible outcome number
+// console.log(Math.ceil(4.3))//it gives greatest roundof number
+
+//console.log(Math.random() * 10 + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
