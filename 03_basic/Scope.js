4c6cd0ae423f81e17e1397ac5c737ca4ba5e1f37
19bcs1715 <19bcs1715@gmail.com>
2024-06-17 15:31:50 +0530

    Scope in JS

--- 03_basic/Scope.js
+++ 03_basic/Scope.js
@@ -1,14 +1,49 @@
-//Global Scope and Local Scope
-let globalvar = " I am global scope"
+// //Global Scope and Local Scope
+// let globalvar = " I am global scope"
 
-function myFunction(){
-  let localvar = "I am local scope"
+// function myFunction(){
+//   let localvar = "I am local scope"
 
-  console.log(globalvar)
-  console.log(localvar)
+//   console.log(globalvar)
+//   console.log(localvar)
+
+// }
+// myFunction();
+// console.log(globalvar)//It is accessible from anywhere in code
+//   //console.log(localvar)//it shows error
+
+
+
+//Nested Scope
+
+function one() {
+  let username = "Bittu"
+
+  function two() {
+    let websitename = " Github"
+    console.log(username)
+
+    if (username === "Bittu") {
+      if (websitename === " Github") {
+        console.log(username + websitename)
+
+
+      }
+
+    }
+  }
+  two();
+}
+one();
+
+console.log(addnum1(5))//it is called hositing
+function addnum1(num) {
+  return num + 1
+}
+
+
+const addnum2 = function (num2) {
+  return num2 + 2
 
 }
-myFunction();
-console.log(globalvar)//It is accessible from anywhere in code
-  //console.log(localvar)//it shows error
-
+console.log(addnum2(5));
