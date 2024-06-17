867f0beca2534b84da220cf5da488b174043f96d
19bcs1715 <19bcs1715@gmail.com>
2024-06-17 22:53:05 +0530

    Arrow in js

--- 03_basic/Arrow.js
+++ 03_basic/Arrow.js
@@ -1,23 +1,35 @@
-const User = {
-  UserName: "Bittu",
-  Password: 1234,
-  WelcomeMessage: function () {
-    console.log(`${this.UserName},Welcome to my Login Page`);
+// const User = {
+//   UserName: "Bittu",
+//   Password: 1234,
+//   WelcomeMessage: function () {
+//     console.log(`${this.UserName},Welcome to my Login Page`);
 
-  }
-
-}
-User.WelcomeMessage()
-User.UserName = "Rahul"
-User.WelcomeMessage()
-
-
-// const chai = function () {
-//   let username = "Bittuu"
-//   console.log(`${this.username}`)
+//   }
 
 // }
-// chai()
+// User.WelcomeMessage()
+// User.UserName = "Rahul"
+// User.WelcomeMessage()
 
-const addnum = (num1, num2)=> (num1 + num2)
-console.log(addnum(3, 4))
+
+// // const chai = function () {
+// //   let username = "Bittuu"
+// //   console.log(`${this.username}`)
+
+// // }
+// // chai()
+
+// const addnum = (num1, num2) => (num1 + num2)
+// console.log(addnum(3, 4))
+
+
+//IIFE(Immediatly Invoked Function Expression)
+
+(function chai() {
+  console.log('Database Connected');
+})();//use semicolon to diffrentiate IIFE 
+
+((Name) => {
+  console.log(`My name is ,${Name}`) // We can not use this method in Arrow function
+}
+)("Bittu")
