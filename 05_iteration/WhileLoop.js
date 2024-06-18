e2ee127851ec7f743758c656d4af0c8718238b07
19bcs1715 <19bcs1715@gmail.com>
2024-06-18 23:22:39 +0530

    While-Do While - Loop

---
+++ 05_iteration/WhileLoop.js
@@ -0,0 +1,26 @@
+//While loop 
+// let index = 0;
+// while (index <=10) {
+//   console.log(`Value of index${index}`)
+//   index = index+2
+  
+// }
+
+//Array in WhileLoop
+// let Myarray = ["Sudir" , "Dilip" , "Rahul" , "Herogama"]
+// let arr = 0;
+// while (arr<Myarray.length) {
+//   console.log(`Name of Superhero ${Myarray[arr]}`)
+//   arr = arr+1
+  
+// }
+
+
+let Score = 0
+do {
+  console.log(`My score is ${Score}`);
+  Score++;
+  
+} while (Score <=10);
+
+
