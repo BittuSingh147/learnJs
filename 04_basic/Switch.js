65e63581e8524cef22a2c36140a45174f10f577a
19bcs1715 <19bcs1715@gmail.com>
2024-06-18 19:45:43 +0530

    Switch case

---
+++ 04_basic/Switch.js
@@ -0,0 +1,19 @@
+const month = "january"
+switch (month) {
+  case "january":
+    console.log("january");
+    break;
+  case "Feb":
+    console.log("February");
+    break;
+  case "March":
+    console.log("March");
+    break;
+  case "April":
+    console.log("April");
+    break;
+
+  default:
+    console.log("No matches");
+    break;
+}
