850424c2bf7fcef25e2e6420f4c88242c6b79c73
19bcs1715 <19bcs1715@gmail.com>
2024-06-27 06:36:37 +0530

    <Practice question>

---
+++ Practice_javascript/index.js
@@ -0,0 +1,54 @@
+// let myVAr = 10;
+// console.log(myVAr);
+
+// function greet(name) {
+//   console.log("Hello" + " "+ name + "!")
+// }
+// greet("Bittu")
+
+// function add2num(x, y) {
+//   return x + y
+// }
+// console.log(add2num(4,6))
+
+
+// function isEven(number) {
+//   if (number % 2 == 0) {
+//     console.log("The number is even");
+
+//   } else {
+//     console.log("the number is odd");
+//   }
+// }
+// isEven(6)
+
+//  function printArray(array){
+//   array.forEach(element => {
+//     console.log(element);
+//   });
+//  }
+// printArray([1,2,3,4,5,6])
+
+// function toUpperCase(str) {
+//   return str.toUpperCase();
+// }
+// console.log(toUpperCase("bittu"))
+
+
+// function toLowerCase(str2) {
+//   return str2.toLowerCase()
+// }
+// console.log(toLowerCase("BITTU"));
+
+
+// function pussh(appenelement) {
+//   return appenelement.push(6)
+// }
+// console.log(pussh([1, 2, 3, 4, 5]));
+
+// const info = {
+//   name: "Bittu",
+//   age: 21
+// }
+// console.log(info);
+
