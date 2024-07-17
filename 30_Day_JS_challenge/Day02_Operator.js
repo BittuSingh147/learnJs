f2c413ad6480de0eed1d6131634517d546176ee3
19bcs1715 <19bcs1715@gmail.com>
2024-07-17 20:17:23 +0530

    Day 2 Operator challenge

---
+++ 30_Day_JS_challenge/Day02_Operator.js
@@ -0,0 +1,45 @@
+//Arithmetic Operators
+//Task 1 to 5 add, subtract , multiply , divisor and reminder of two numbers
+let num1 = 2;
+num2 = 4;
+divident = 30;
+divisor = 3.5;
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(divident % divisor);
+
+//Assignment operators
+//Task 6 to Task 7 (-= , +=)
+let nums1 = 4;
+nums2 = 6;
+console.log(nums1 -= nums2);
+console.log(nums1 += nums2);
+
+//Comparision Operators
+//Task 8 to task 10 <,>,<= , >= , == ,===
+let numbers1 = 4;
+numbers2 = 8;
+console.log(numbers1 < numbers2);
+console.log(numbers1 > numbers2);
+console.log(numbers1 >= numbers2);
+console.log(numbers1 <= numbers2);
+console.log(numbers1 == numbers2);
+console.log(numbers1 === numbers2);
+
+//Logical Operators
+//Task 11to 13  && || and !
+console.log(numbers1 && numbers2);
+console.log(numbers1 || numbers2);
+
+console.log(!numbers1);
+console.log(!numbers2);
+
+
+//Ternary Operators 
+//Task 15
+let age = 16;
+canvote = age >= 18 ? 'yes' : 'No'
+console.log(canvote);
+
